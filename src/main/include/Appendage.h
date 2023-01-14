8d87a44e5b4336f001d39f207d6cf9678a5e9720
savage301 <savage301@gmail.com>
2023-01-14 14:43:17 -0500

    Add Appendage files

---
+++ src/main/include/Appendage.h
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <numbers>
+
+
+/**
+ * Represents a swerve drive style drivetrain.
+ */
+class Appendage {
+ public:
+  Appendage() { }
+ 
+
+ private:
+  
+};
