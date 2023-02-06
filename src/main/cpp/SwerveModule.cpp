d90a734cfc84f4ecfbef5f2a7dd325f1e2308178
PIPIPIG233666 <pig.priv@gmail.com>
2023-02-05 20:53:20 -0500

    update license

--- src/main/cpp/SwerveModule.cpp
+++ src/main/cpp/SwerveModule.cpp
@@ -1,7 +1,7 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2023 FIRST and other WPILib contributors.
+// Copyright (c) 2023 FRC Team 573
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-// Copyright (c) 2023 FRC Team 573
 
 #include "SwerveModule.h"
 
