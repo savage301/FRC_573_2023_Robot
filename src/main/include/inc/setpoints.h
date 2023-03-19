2f04485e0b91549d7559a9b7f1d01bedd9b64fc3
PIPIPIG233666 <pig.priv@gmail.com>
2023-03-19 10:57:23 -0400

    sort setpts

--- src/main/include/inc/setpoints.h
+++ src/main/include/inc/setpoints.h
@@ -4,42 +4,54 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #pragma once
-// Appendage Encoder Limits
-const double shoulder_max = 1700;
-const double shoulder_min = -2000.0;
 
+// Appendage Encoder Limits
 const double arm_max = -5;
 const double arm_min = -125.0;
 
-const double wrist_max = 10000.0;
+const double shoulder_max = 1700;
+const double shoulder_min = -2000.0;
+
+const double wrist_max = 4000.0;
 const double wrist_min = 0.0;
 
+// home
 const double armHome = 10;
-// const double armFloor = ;
-// const double armMid = ;
-const double armHighCube = -112;
-const double armHighCone = -115;
-const double armHumanHigh = 10;
-
 const double shoulderHome = 0;
-const double shoulderFloor = -1994;
-const double shoulderMidCube = -1150;
-const double shoulderMidCone = -540;
-const double shoulderHighCube = -800;
-const double shoulderHighCone = -650;
-const double shoulderHumanLow = 1600;
-const double shoulderHumanHigh = 535;
-
 const double wristHome = 100;
-const double wristFloorCubeScore = 100;
-const double wristFloorConeScore = 100;
+
+// floor pickup
+// const double armFloor = ;
+const double shoulderFloor = -1994;
 const double wristFloorCubeLoad = 1900;
 const double wristFloorConeLoad = 1539;
 const double wristFloor = 1326;
+
+// floor score
+const double wristFloorCubeScore = wristHome;
+const double wristFloorConeScore = wristHome;
+
+// mid score
+const double armMidCube = armHome;
+const double shoulderMidCube = -1150;
+const double shoulderMidCone = -540;
 const double wristMidCone = 2200;
-const double wristMidCube = 100;
+const double wristMidCube = wristHome;
+
+// high score
+const double armHighCube = -112;
+const double armHighCone = -115;
+const double shoulderHighCube = -800;
+const double shoulderHighCone = -650;
 const double wristHigh = 2445;
-const double wristHighCube = 100;
+const double wristHighCube = wristHome;
 const double wristHighCone = 2445;
-const double wristHumanLow = 100;
+
+// high human player
+const double armHumanHigh = armHome;
+const double shoulderHumanHigh = 535;
 const double wristHumanHigh = 3700;
+
+// low human player
+const double shoulderHumanLow = 1600;
+const double wristHumanLow = wristHome;
